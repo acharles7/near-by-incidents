69bba957f33967b9f81b79574e17614895fd28cd
Charles <lomesh@CHARLESs-MacBook-Pro.local>
2019-01-19 02:50:27 -0800

    Update SOS Tab

--- app/components/DrawerComponents/SOS.js
+++ app/components/DrawerComponents/SOS.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {
     Text,
+    TouchableOpacity,
     View
 } from 'react-native';
-
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class SOS extends Component {
@@ -12,7 +12,7 @@ class SOS extends Component {
         return {
             headerLeft: (
                 <View style={{padding: 10}}>
-                    <Ionicons name="md-menu" 
+                    <Ionicons name="md-menu"
                         size={24}
                         onPress={() => navigation.navigate('DrawerOpen')}
                     />
@@ -23,9 +23,20 @@ class SOS extends Component {
 
     render(){
         return (
-            <View>
-                <Text> SOS Screen </Text>
-            </View>
+          <TouchableOpacity
+             style={{
+                 borderWidth:1,
+                 borderColor:'rgba(0,0,0,0.2)',
+                 alignItems:'center',
+                 justifyContent:'center',
+                 width:100,
+                 height:100,
+                 color:'#f1f1f1',
+                 backgroundColor:'#fff',
+                 borderRadius:100,
+               }}>
+             <Ionicons name={"chevron-right"}  size={50} color="#01a699" />
+           </TouchableOpacity>
         )
     }
 }
